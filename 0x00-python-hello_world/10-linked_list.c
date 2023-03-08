b485706e94cf17704c7a30acfa82cc33865bfbde
ken00alx <kenny933@gmail.com>
2023-03-07 16:44:13 -0800

    updated

--- 0x00-python-hello_world/10-linked_list.c
+++ 0x00-python-hello_world/10-linked_list.c
@@ -1,63 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
-
-/**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
-
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
-
-	return (n);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	listint_t *current;
-
-	while (head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
-}
+#include "lists.h" 
+ #include <stdlib.h> 
+  
+ /** 
+  * _realloc - Reallocates a memory block 
+  * @ptr: The pointer to the previous memory block 
+  * @old_size: The size of the old memory block 
+  * @new_size: The size of the new memory block 
+  * 
+  * Return: The pointer to the new memory block otherwise NULL 
+  */ 
+ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) 
+ { 
+         void *new_ptr; 
+         unsigned int min_size = old_size < new_size ? old_size : new_size; 
+         unsigned int i; 
+  
+         if (new_size == old_size) 
+                 return (ptr); 
+         if (ptr != NULL) 
+         { 
+                 if (new_size == 0) 
+                 { 
+                         free(ptr); 
+                         return (NULL); 
+                 } 
+                 new_ptr = malloc(new_size); 
+                 if (new_ptr != NULL) 
+                 { 
+                         for (i = 0; i < min_size; i++) 
+                                 *((char *)new_ptr + i) = *((char *)ptr + i); 
+                         free(ptr); 
+                         return (new_ptr); 
+                 } 
+                 free(ptr); 
+                 return (NULL); 
+         } 
+         else 
+         { 
+                 new_ptr = malloc(new_size); 
+                 return (new_ptr); 
+         } 
+ }
---
+++ README.md
@@ -0,0 +1,1 @@
+Creating an Hello World script
